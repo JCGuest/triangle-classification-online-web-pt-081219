fba48643818c7d60b2db02e44fee9376de135605
John Guest <jcguest90@gmail.com>
2019-09-25 05:32:27 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,9 +11,9 @@ class Triangle
   def kind 
     if @x == @y && @z 
       :equilateral
-    else @x != @y && @z
+    else @x != @y && @z || @x && @y != @z
       :isosceles
-    else @x && @y != z
+    end 
   end
   
   
