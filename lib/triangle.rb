0821beb7fa6897fbaaab93df4219b1b2d6b93d83
John Guest <jcguest90@gmail.com>
2019-09-25 05:26:05 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,10 +1,16 @@
 class Triangle
   
-  attr_accessor :equalateral, :isosceles, 
+  attr_accessor :equalateral, :isosceles, :scalene
   
   def initialize(x,y,z)
     @x = x
     @y = y 
     @z = z
+  end
+  
+  def kind 
+    if @x == @y == @z 
+      :equalateral
+    end
   end
 end 
